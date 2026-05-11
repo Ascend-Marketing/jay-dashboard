390c168fa9497793ec3c1e9029b86851f386f1ae
Ascend-Marketing <jaywilliams.jw5@gmail.com>
2026-05-11 14:11:25 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,16 @@
+import Head from "next/head";
 import Dashboard from "../components/Dashboard";
+
 export default function Home() {
-  return <Dashboard />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <title>Jay Dashboard</title>
+      </Head>
+      <Dashboard />
+    </>
+  );
 }
